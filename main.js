dd9350ea3b68d8024fd7a410d1c7a0f96ed69d9e
“ann” <136979138+ann328@users.noreply.github.com>
2023-12-01 23:14:23 +0800

    fix: 🐛 fix environment

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
 import "./assets/scss/all.scss";
 
-import "./assets/js/indexVersion.js";
-
-import "./assets/js/adminVersion.js";
-
-import "./assets/js/index.js";
-
-import "./assets/js/admin.js";
-
 console.log("Hello world!");
