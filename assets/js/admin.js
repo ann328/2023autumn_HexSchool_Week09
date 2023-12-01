6d6513859e2eb337e74fa431c251ad72dfcd35c6
“ann” <136979138+ann328@users.noreply.github.com>
2023-12-01 22:25:25 +0800

    fix: 🐛 fix environment

---
+++ assets/js/admin.js
@@ -0,0 +1,18 @@
+let chart = c3.generate({
+  bindto: "#chart", // HTML 元素綁定
+  data: {
+    type: "pie",
+    columns: [
+      ["Louvre 雙人床架", 1],
+      ["Antony 雙人床架", 2],
+      ["Anty 雙人床架", 3],
+      ["其他", 4],
+    ],
+    colors: {
+      "Louvre 雙人床架": "#DACBFF",
+      "Antony 雙人床架": "#9D7FEA",
+      "Anty 雙人床架": "#5434A7",
+      其他: "#301E5F",
+    },
+  },
+});
--- main.js
+++ main.js
@@ -6,4 +6,6 @@ import "./assets/js/adminVersion.js";
 
 import "./assets/js/index.js";
 
+import "./assets/js/admin.js";
+
 console.log("Hello world!");
